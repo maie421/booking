a2fd6c4f7021919bc479175ceb12cae4d3d034dc
윤영미 <maie421@naver.com>
2022-11-13 20:57:43 +0900

    feat : pdo

--- class/db_connect.php
+++ class/db_connect.php
@@ -2,9 +2,7 @@
 
 class DB_CONNECT
 {
-    private $dbHost = "127.0.0.1";    // 호스트 주소(localhost, 120.0.0.1)
-    private $dbName = "booking";      // 데이타 베이스(DataBase) 이름
-    private $user = "yeongmi"; // DB 아이디
+    private $user = "root"; // DB 아이디
     private $password = "1234"; // DB 패스워드
 
     public $pdo;
@@ -12,7 +10,7 @@ class DB_CONNECT
     {
         try {
             // 서버 이름, 데이터베이스 이름, 사용자명과 비밀번호를 전달하여 새로운 PDO 객체를 생성
-            $this->pdo = new PDO('mysql:host=192.168.64.1', $this->user, $this->password);
+            $this->pdo = new PDO('mysql:host=host.docker.internal', $this->user, $this->password);
             // 생성된 PDO 객체에 에러 모드(error mode)를 설정
             // 이렇게 에러 모드를 설정하면, PDO 생성자는 에러가 발생할 때마다 PDOException 예외를 던질 것이다.
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
