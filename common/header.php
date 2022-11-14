42eaffbac2bde02ebe95b46a874b4c71aef69d82
윤영미 <maie421@naver.com>
2022-11-14 16:57:20 +0900

    feat : modal form css

--- common/header.php
+++ common/header.php
@@ -1,5 +1,6 @@
 <?php
-    require_once dirname(__FILE__,2).'/vendor/autoload.php';
+
+require_once dirname(__FILE__, 2).'/vendor/autoload.php';
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,7 +10,9 @@
     <title>Booking</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
@@ -18,7 +21,7 @@
 <body>
 <div class="container">
     <div class="d-flex justify-content-between">
-        <img src="../img/logo.png" height="100" width="150" />
+        <img src="../img/logo.png" height="100" width="150"/>
         <div class="align-self-center">
             <div class="input-group  mx-sm-3">
                 <input type="text" class="form-control" placeholder="검색 시작하기"
@@ -45,20 +48,24 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-body">
-                <button type="button" class="btn-close btn-close-black" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close btn-close-black" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
                 <div class="myform">
                     <h1 class="text-center">로그인</h1>
                     <form>
                         <div class="mb-3 mt-4">
                             이메일
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                   aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
                             패스워드
                             <input type="password" class="form-control" id="exampleInputPassword1">
                         </div>
-                        <button type="submit" class="btn btn-light mt-3">LOGIN</button>
-                        <p class="mt-3"><a data-bs-toggle="modal" data-bs-target="#JoinForm">회원가입</a></p>
+                        <div class="d-grid gap-2 col-6 mx-auto mt-3">
+                            <button type="submit" class="btn btn-primary mt-3">로그인</button>
+                        </div>
+                        <!--                        <p class="mt-3"><a data-bs-toggle="modal" data-bs-target="#JoinForm">회원가입</a></p>-->
                     </form>
                 </div>
             </div>
@@ -72,23 +79,28 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-body">
-                <button type="button" class="btn-close btn-close-black" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close btn-close-black" data-bs-dismiss="modal"
+                        aria-label="Close"></button>
                 <div class="myform">
                     <h1 class="text-center">회원가입</h1>
                     <form>
                         <div class="mb-3 mt-4">
                             이메일
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                   aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3 mt-4">
                             닉네임
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                   aria-describedby="emailHelp">
                         </div>
                         <div class="mb-3">
                             패스워드
                             <input type="password" class="form-control" id="exampleInputPassword1">
                         </div>
-                        <button type="submit" class="btn btn-light mt-3">확인</button>
+                        <div class="d-grid gap-2 col-6 mx-auto mt-3">
+                            <button type="submit" class="btn btn-primary mt-3">확인</button>
+                        </div>
                     </form>
                 </div>
             </div>
