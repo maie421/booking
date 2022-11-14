c4f362d5eff7f52f8325b994512d7259093ce3e9
윤영미 <maie421@naver.com>
2022-11-14 19:28:47 +0900

    feat : 관리자 페이지 대시보드 예약 관리 ui

--- admin/order.php
+++ admin/order.php
@@ -26,7 +26,8 @@
 <div class="d-flex bd-highlight">
     <div class="p-2 bd-highligh" style="width:80%;" id="calendar"></div>
     <div class="d-flex flex-column bd-highlight mb-3">
-        <div class="card" style="width: 20rem;">
+        <?php for ($i = 0; $i < 3; $i++){?>
+        <div class="card mb-3" style="width: 20rem;">
             <div class="card-body">
                 <h5 class="card-title">해운대</h5>
                 <p class="card-text">윤영미</p>
@@ -38,18 +39,7 @@
                 </div>
             </div>
         </div>
-        <div class="card" style="width: 20rem;">
-            <div class="card-body">
-                <h5 class="card-title">해운대</h5>
-                <p class="card-text">윤영미</p>
-                <p class="card-text">010-0000-0000</p>
-                <p class="card-text">예약 기간 : 2022-11-14 ~ 2022-11-15</p>
-                <div class="d-flex bd-highlight">
-                    <button type="button" class="btn btn-outline-dark" style="margin-right: 5px">예약 수정</button>
-                    <button type="button" class="btn btn-outline-dark mr-3">예약 취소</button>
-                </div>
-            </div>
-        </div>
+        <?}?>
     </div>
 </div>
 <script type="text/javascript">
@@ -66,22 +56,22 @@
             selectHelper:true,
             select: function(start, end, allDay)
             {
-                var title = prompt("Enter Event Title");
-                if(title)
-                {
-                    var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-                    var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
-                    $.ajax({
-                        url:"insert.php",
-                        type:"POST",
-                        data:{title:title, start:start, end:end},
-                        success:function()
-                        {
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Added Successfully");
-                        }
-                    })
-                }
+                // var title = prompt("Enter Event Title");
+                // if(title)
+                // {
+                //     var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+                //     var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+                //     $.ajax({
+                //         url:"insert.php",
+                //         type:"POST",
+                //         data:{title:title, start:start, end:end},
+                //         success:function()
+                //         {
+                //             calendar.fullCalendar('refetchEvents');
+                //             alert("Added Successfully");
+                //         }
+                //     })
+                // }
             },
             editable:true,
             eventResize:function(event)
