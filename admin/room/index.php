c26a7335119304cb8921f2ef2b4a6c3231802b55
윤영미 <maie421@naver.com>
2022-11-19 00:28:34 +0900

    feat : room 출력

--- admin/room/index.php
+++ admin/room/index.php
@@ -15,20 +15,21 @@ $row = $room->getRoom();
                 <a href="add.php" class="btn btn-primary me-md-2 mb-3" type="button">추가</a>
             </div>
             <?php
-//           foreach ($row as $key => $value){
-           foreach ($row as $key => $value){?>
-            <div class="d-flex border p-3 mb-3">
-                <img src="../../img/room/<?=$value['img']?>"
-                     class="img-thumbnail" width="200">
-                <div class="card-body ms-3">
-                    <h5><?=$value['name']?></h5>
-                    <p class="card-text">최대 인원 : <?=$value['max_people']?>명 </p>
-                    <p class="card-text">가격 : <?=floor($value['price'])?> /박</p>
-                    <a href="add.php" type="button" class="btn btn-outline-dark me-2">수정</a>
-                    <a type="button" class="btn btn-outline-dark">삭제</a>
+            foreach ($row as $key => $value) {
+                ?>
+                <div class="d-flex border p-3 mb-3">
+                    <img src="../../img/room/<?= $value['img'] ?>"
+                         class="img-thumbnail" width="200">
+                    <div class="card-body ms-3">
+                        <h5><?= $value['name'] ?></h5>
+                        <p class="card-text">최대 인원 : <?= $value['max_people'] ?>명 </p>
+                        <p class="card-text">가격 : <?= floor($value['price']) ?> /박</p>
+                        <a href="add.php" type="button" class="btn btn-outline-dark me-2">수정</a>
+                        <a type="button" class="btn btn-outline-dark">삭제</a>
+                    </div>
                 </div>
-            </div>
-            <?}?>
+                <?php
+            } ?>
         </div>
     </div>
 </div>
